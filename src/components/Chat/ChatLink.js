b0c6ffb29b4d4cb2b0052e6f615dba4ca0b4aebe
Ubaton <raymondNG729@gmail.com>
2023-10-22 07:13:37 +0200

    v0.1.56

--- src/components/Chat/ChatLink.js
+++ src/components/Chat/ChatLink.js
@@ -8,7 +8,34 @@ const FloatingChatIcon = () => {
         target="_blank"
         className="z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange"
       >
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABzElEQVR4nN3VTYhNcRgG8Ds+QtI03dkpiyGriZCNhS7WEgsbdizI0sKU8jEr1CgLoRQjJEUiG4qdlLvCZhZjJSSx8Hl9/fTXG/+Oc++ckxVvncV57vN/n/t/3rfnNBr/fWE5RnEDD3APV7EbQ3/TeAXu+l0vQiAJPcR3fMN5LKjbfDs6+IgxDJdw5sfN3uEV1lRtvi3+8WMsqsAfwiN8wqoqtnSieX9gTVzD7ey5iE3ZuQFMhI3NXgJ3wpaFGdaKG7UzgcnA9ma8JfiKo92aL4tDYwW8FXgrw6ZhHJ8xL8PP4APmlgkcjEbDUwmkwvrAV2bY2sA2NoqF68nDErzV5Qbn4ga/PMfMsGm0TCDteLuHQDubwZPA9pXwn+N0N4H7FQUmA9tfwn+D42UCaRWf1bBoPCwazPD+4O4pExiJHxdPJdBjyJsDW9coVtr9yJYjNYbcKazpZTzF9D8EgnAF7zG74pBHCue3lq5oRriFl5iRYc2SqLiADV0bdWk+iC84Fe9zeuZK3YqI/hkVEWZvI5eOYSn6Mm4fVuMkdlUVuJl9XNKeH8aJiOFUr+NDM1HAdlYV2IFDKbIL+AC2pJTEJZzFgTRMzKpt1T9XPwDmoDzX8b/nmAAAAABJRU5ErkJggg==" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="24"
+          height="24"
+          viewBox="0,0,256,256"
+        >
+          <g
+            fill="#ffffff"
+            fillRule="evenodd"
+            stroke="none"
+            strokeWidth="1"
+            strokeLinecap="butt"
+            strokeLinejoin="miter"
+            strokeMiterlimit="10"
+            strokeDasharray=""
+            strokeDashoffset="0"
+            fontFamily="none"
+            fontWeight="none"
+            fontSize="none"
+            textAnchor="none"
+          >
+            <g transform="scale(8,8)">
+              <path d="M24.50391,7.50391c-2.25781,-2.25781 -5.25781,-3.50391 -8.45312,-3.50391c-6.58594,0 -11.94922,5.35938 -11.94922,11.94531c-0.00391,2.10547 0.54688,4.16016 1.59375,5.97266l-1.69531,6.19141l6.33594,-1.66406c1.74219,0.95313 3.71094,1.45313 5.71094,1.45703h0.00391c6.58594,0 11.94531,-5.35937 11.94922,-11.94922c0,-3.19141 -1.24219,-6.19141 -3.49609,-8.44922zM16.05078,25.88281h-0.00391c-1.78125,0 -3.53125,-0.48047 -5.05469,-1.38281l-0.36328,-0.21484l-3.76172,0.98438l1.00391,-3.66406l-0.23437,-0.375c-0.99609,-1.58203 -1.51953,-3.41016 -1.51953,-5.28516c0,-5.47266 4.45703,-9.92578 9.9375,-9.92578c2.65234,0 5.14453,1.03516 7.01953,2.91016c1.875,1.87891 2.90625,4.37109 2.90625,7.02344c0,5.47656 -4.45703,9.92969 -9.92969,9.92969zM21.49609,18.44531c-0.29687,-0.14844 -1.76562,-0.87109 -2.03906,-0.96875c-0.27344,-0.10156 -0.47266,-0.14844 -0.67187,0.14844c-0.19922,0.30078 -0.76953,0.97266 -0.94531,1.17188c-0.17187,0.19531 -0.34766,0.22266 -0.64453,0.07422c-0.30078,-0.14844 -1.26172,-0.46484 -2.40234,-1.48437c-0.88672,-0.78906 -1.48828,-1.76953 -1.66016,-2.06641c-0.17578,-0.30078 -0.01953,-0.46094 0.12891,-0.60937c0.13672,-0.13281 0.30078,-0.34766 0.44922,-0.52344c0.14844,-0.17187 0.19922,-0.29687 0.30078,-0.49609c0.09766,-0.19922 0.04688,-0.375 -0.02734,-0.52344c-0.07422,-0.14844 -0.67187,-1.62109 -0.92187,-2.21875c-0.24219,-0.58203 -0.48828,-0.5 -0.67187,-0.51172c-0.17187,-0.00781 -0.37109,-0.00781 -0.57031,-0.00781c-0.19922,0 -0.52344,0.07422 -0.79687,0.375c-0.27344,0.29688 -1.04297,1.01953 -1.04297,2.48828c0,1.46875 1.07031,2.89063 1.21875,3.08984c0.14844,0.19531 2.10547,3.21094 5.10156,4.50391c0.71094,0.30859 1.26563,0.49219 1.69922,0.62891c0.71484,0.22656 1.36719,0.19531 1.88281,0.12109c0.57422,-0.08594 1.76563,-0.72266 2.01563,-1.42187c0.24609,-0.69531 0.24609,-1.29297 0.17188,-1.41797c-0.07422,-0.125 -0.27344,-0.19922 -0.57422,-0.35156z"></path>
+            </g>
+          </g>
+        </svg>
       </a>
     </div>
   );
